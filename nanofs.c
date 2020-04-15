0a5b76324e31653425f6c65c6c5bb521d7510ef6
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-04-15 04:55:20 +0200

    initial release

--- nanofs.c
+++ nanofs.c
@@ -1,21 +1,21 @@
 
 /*
- *  Copyright 2016-2020 Alejandro Calderon Mateos
+ *  Copyright 2016-2020 Alejandro Calderon Mateos (ARCOS.INF.UC3M.ES)
  *
  *  This file is part of nanofs (nano-filesystem).
  *
- *  WepSIM is free software: you can redistribute it and/or modify
+ *  nanofs is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  WepSIM is distributed in the hope that it will be useful,
+ *  nanofs is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with WepSIM.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with nanofs.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
